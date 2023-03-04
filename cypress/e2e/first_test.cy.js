cbb69460c14501cd9e1a47adabaeba471640ed17
cristian ayala <cristian.adrian1993@gmail.com>
2023-03-03 23:56:58 -0300

    First test with Cypress - Title validation

--- cypress/e2e/first_test.cy.js
+++ cypress/e2e/first_test.cy.js
@@ -1,5 +1,19 @@
 describe('First test whit Cypress', () => {
-  it('passes', () => {
-    cy.visit('https://amazon.com')
+  it('verify title-positive', () => {
+    //Steps 
+    //Ingreso al sitio web
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+    //Se verifica el título del sitio
+    cy.title().should('eq','OrangeHRM')
+
+  })
+  it('verify title-negative', () => {
+    //Steps 
+    //Ingreso al sitio web
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+    //Se verifica el título del sitio 
+    cy.title().should('eq','OrangeHRMTest')
   })
 })
